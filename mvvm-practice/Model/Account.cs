b27287f2696e87be1e9ee855bec3ee30db316741
Maksim Filipiuk <maksim.filipiuk@gmail.com>
2019-01-15 19:58:37 +0200

    Created class Account and AccountRepository

--- mvvm-practice/Model/Account.cs
+++ mvvm-practice/Model/Account.cs
@@ -8,6 +8,39 @@ namespace mvvm_practice.Model
 {
     class Account
     {
+        private int id;
+        private string login;
+        private string password;
+        private string email;
+        private DateTime birthdayDate;
 
+        public int Id { get => id; set => id = value; }
+        public string Login { get => login; set => login = value; }
+        public string Password { get => password; set => password = value; }
+        public string Email { get => email; set => email = value; }
+        public DateTime BirthdayDate { get => birthdayDate; set => birthdayDate = value; }
+
+        public Account()
+        {
+        }
+
+        public Account(int id, string login, string password)
+        {
+            this.id = id;
+            this.login = login;
+            this.password = password;
+        }
+
+        public Account(int id, string login, string password, string email)
+            : this(id, login, password)
+        {
+            this.email = email;
+        }
+
+        public Account(int id, string login, string password, string email, DateTime birthdayDate)
+            : this(id, login, password, email)
+        {
+            this.birthdayDate = birthdayDate;
+        }
     }
 }
--- mvvm-practice/Model/AccountRepository.cs
+++ mvvm-practice/Model/AccountRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,27 @@ namespace mvvm_practice.Model
 {
     class AccountRepository
     {
+        private ObservableCollection<Account> accounts;
+
+        public ObservableCollection<Account> Accounts
+        {
+            get
+            {
+                if(accounts == null)
+                    accounts = LoadAccounts();
+                return accounts;
+            }
+        }
+
+        private ObservableCollection<Account> LoadAccounts() // тут может быть загрузка аккаунтов с БД
+        {
+            ObservableCollection<Account> _accounts = new ObservableCollection<Account>();
+
+            _accounts.Add(new Account(1, "devmaks", "12345"));
+            _accounts.Add(new Account(2, "artem", "qwerty", "lol@lol.lol"));
+            _accounts.Add(new Account(3, "nasya","xexex","cherry@cher.ry", new DateTime(1997, 8, 4)));
+
+            return _accounts;
+        }
     }
 }
