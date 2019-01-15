098068c70f394c072da9a18febaac07cde029682
Maksim Filipiuk <maksim.filipiuk@gmail.com>
2019-01-15 19:38:35 +0200

    Initialization MVVM structure

---
+++ Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvm_practice.Model
+{
+    class Account
+    {
+        private int id;
+        private string login;
+        private string password;
+        private string email;
+        private DateTime birthdayDate;
+
+        public int Id { get => id; set => id = value; }
+        public string Login { get => login; set => login = value; }
+        public string Password { get => password; set => password = value; }
+        public string Email { get => email; set => email = value; }
+        public DateTime BirthdayDate { get => birthdayDate; set => birthdayDate = value; }
+
+        public Account()
+        {
+        }
+
+        public Account(int id, string login, string password)
+        {
+            this.id = id;
+            this.login = login;
+            this.password = password;
+        }
+
+        public Account(int id, string login, string password, string email) 
+            : this(id, login, password)
+        {
+            this.email = email;
+        }
+
+        public Account(int id, string login, string password, string email, DateTime birthdayDate) 
+            : this(id, login, password, email)
+        {
+            this.birthdayDate = birthdayDate;
+        }
+    }
+}
---
+++ mvvm-practice/ViewModel/ViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvm_practice.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged; // для обеспечения корректной работы мультипоточности
+
+            if(handler != null)
+                handler.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Dispose()
+        {
+            this.OnDispose();
+        }
+
+        protected virtual void OnDispose() // для очистки неуправляемых ресурсов (например, коллекций)
+        {
+        }
+    }
+}
